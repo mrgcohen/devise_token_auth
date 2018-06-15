66b98c5842626b3ffa053eb474ca6a6405964c78
Michael Cohen <mrgcohen@gmail.com>
2018-06-14 23:26:43 -0400

    customize

--- app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
+++ app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
@@ -2,12 +2,12 @@ module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
   extend ActiveSupport::Concern
 
   included do
-    validates :email, presence: true,if: :email_provider?
+    validates :email, presence: true, if: :email_provider?
     validates :email, email: true, allow_nil: true, allow_blank: true, if: :email_provider?
     validates_presence_of :uid, unless: :email_provider?
 
     # only validate unique emails among email registration users
-    validates :email, uniqueness: { scope: :provider }, on: :create, if: :email_provider?
+    validates :email, uniqueness: { scope: [:provider, :organization_id] }, on: :create, if: :email_provider?
 
     # keep uid in sync with email
     before_save :sync_uid
